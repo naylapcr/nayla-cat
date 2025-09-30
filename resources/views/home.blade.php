589c79594b4f831cc3ea79869ab8665fd4503ec2
naylapcr <nayla24si@mahasiswa.pcr.ac.id>
2025-09-30 10:42:22 +0700

    penambahan respon form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -169,7 +169,7 @@
             </div>
             <div class="mb-3">
                 <label for="pertanyaan" class="form-label">Pertanyaan</label></label></label>
-                <textarea class="form-control" rows="4"></textarea>
+                <textarea class="form-control" rows="4" name="pertanyaan"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Kirim Pertanyaan</button>
         </form>
